b421fd93e16a88235263036230d3cb4bcdaa995a
Alex Fall <alex.fall@bluemedora.com>
2020-06-22 18:37:46 -0400

    rest framework

--- startgame/views.py
+++ startgame/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    # Get an HttpRequest - the request parameter
+    # perform operations using information from the request.
+    # Return HttpResponse
+    return HttpResponse('Hello from Django!')
