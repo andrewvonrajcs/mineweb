9d9a8e8baca921ecf936d56029a12ab38f401e54
Alex Fall <alex@fallalex.com>
2020-06-28 22:31:58 -0400

    basic error check and renames

--- minesite/startgame/models.py
+++ minesite/startgame/models.py
@@ -1,26 +1,32 @@
 from django.db import models
 from mcstatus import MinecraftServer
 
-class minecraftServer(models.Model):
+class minecraftStatus(models.Model):
     # TODO: add query to get online player usernames
 
     def __init__(self):
-        self.server = MinecraftServer("minecraft-1.labfall.com", 25565) 
+        self.server = MinecraftServer("minecraft-1", 25565) 
 
     def reload(self):
-        # could add empty str defaults for description and the like
-        self.ping()
-        self.status()
+        try:
+            self.ping()
+            self.status()
+            self.onlinePlayers()
+            return True
+        except Exception as e:
+            self.error = e
+        return False
 
     def status(self):
-        # handle errors
         raw = self.server.status().raw
         self.description = raw['description']['text']
         self.players = raw['players']['online']
         self.version = raw['version']['name']
+    
+    def onlinePlayers (self):
+        self.playerNames = self.server.query().players.names
 
     def ping(self):
-        # handle errors
         self.latency = self.server.ping() 
     
     def get_absolute_url(self):
